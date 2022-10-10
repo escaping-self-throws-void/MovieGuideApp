104e5fd3a057bf88f6711d80e12240586993977d
Paul Matar <paul_matar@icloud.com>
2022-10-10 16:28:49 +0300

    add space for floating label between borders

--- MBC Movie Guide/UIComponents/Views/MBCTextField.swift
+++ MBC Movie Guide/UIComponents/Views/MBCTextField.swift
@@ -12,8 +12,7 @@ final class MBCTextField: UITextField {
     lazy var floatingLabel: UILabel = {
         let label = UILabel()
         label.backgroundColor = .clear
-        label.layer.zPosition = 1
-        label.textAlignment = .center
+        label.textAlignment = .defaultAlignment
         label.font = .init(name: C.Fonts.almaraiRegular, size: 15)
         label.textColor = .init(named: C.Colors.veryLightPink)?.withAlphaComponent(0.5)
         return label
@@ -21,22 +20,66 @@ final class MBCTextField: UITextField {
     
     private var isUp: Bool = false {
         didSet {
+            animateBorders(isUp)
             moveLabel(isUp)
         }
     }
     
-    private let padding = UIEdgeInsets(top: 0, left: 7, bottom: 0, right: 7)
+    private let padding: CGFloat = 7
 
+    private lazy var defaultPath: UIBezierPath = {
+        let path = UIBezierPath()
+        let radius: CGFloat = 4
+        
+        path.move(to: CGPoint(x: 0, y: bounds.height - radius))
+        path.addArc(withCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle: CGFloat.pi, endAngle: CGFloat.pi * 3 / 2, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: bounds.width - radius, y: radius), radius: radius, startAngle: CGFloat.pi * 3 / 2, endAngle: 0, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: bounds.width - radius, y: bounds.height - radius), radius: radius, startAngle: 0, endAngle: .pi / 2, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: radius, y: bounds.height - radius), radius: radius, startAngle: .pi / 2, endAngle: .pi, clockwise: true)
+        path.close()
+        
+        return path
+    }()
+    
+    private lazy var floatingPath: UIBezierPath = {
+        let path = UIBezierPath()
+        let radius: CGFloat = 4
+
+        path.move(to: CGPoint(x: 0, y: bounds.height - radius))
+        path.addArc(withCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle: CGFloat.pi, endAngle: CGFloat.pi * 3 / 2, clockwise: true)
+        let minXOffset = floatingLabel.frame.width * 0.02
+        path.addLine(to: .init(x: padding + minXOffset - 3, y: 0))
+        path.addClip()
+        let maxXOffset = floatingLabel.frame.maxX - (floatingLabel.frame.width * 0.18)
+        path.move(to: .init(x: maxXOffset + 3, y: 0))
+        path.addArc(withCenter: CGPoint(x: bounds.width - radius, y: radius), radius: radius, startAngle: CGFloat.pi * 3 / 2, endAngle: 0, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: bounds.width - radius, y: bounds.height - radius), radius: radius, startAngle: 0, endAngle: .pi / 2, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: radius, y: bounds.height - radius), radius: radius, startAngle: .pi / 2, endAngle: .pi, clockwise: true)
+        
+        return path
+    }()
+    
+    private lazy var borderLayer: CAShapeLayer = {
+        let borderLayer = CAShapeLayer()
+        
+        borderLayer.path = defaultPath.cgPath
+        borderLayer.lineWidth = 1
+        borderLayer.strokeColor = UIColor(named: C.Colors.brownishGreyTwo).unwrap.cgColor
+        borderLayer.fillColor = UIColor.clear.cgColor
+        
+        return borderLayer
+    }()
+    
     override func textRect(forBounds bounds: CGRect) -> CGRect {
-        bounds.inset(by: padding)
+        bounds.inset(by: .init(top: 0, left: padding, bottom: 0, right: padding))
     }
 
     override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-        bounds.inset(by: padding)
+        bounds.inset(by: .init(top: 0, left: padding, bottom: 0, right: padding))
     }
 
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        bounds.inset(by: padding)
+        bounds.inset(by: .init(top: 0, left: padding, bottom: 0, right: padding))
     }
     
     override init(frame: CGRect) {
@@ -45,18 +88,18 @@ final class MBCTextField: UITextField {
         bindFloatingLabel()
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func draw(_ rect: CGRect) {
         super.draw(rect)
-        drawBezierPath()
+        layer.addSublayer(borderLayer)
     }
     
     convenience init(_ placeholder: String) {
         self.init()
         floatingLabel.text = placeholder
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     private func createUI() {
@@ -66,16 +109,13 @@ final class MBCTextField: UITextField {
 
         floatingLabel.place(on: self).pin(
             .centerY(),
-            .leading(to: self, padding: 7),
-            .fixedWidth(50)
+            .leading(to: self, padding: padding)
         )
-        
-        bringSubviewToFront(floatingLabel)
     }
     
     private func bindFloatingLabel() {
-        addTarget(self, action: #selector(start), for: .editingDidBegin)
-        addTarget(self, action: #selector(end), for: .editingDidEnd)
+        addTarget(self, action: #selector(startFloating), for: .editingDidBegin)
+        addTarget(self, action: #selector(endFloating), for: .editingDidEnd)
     }
     
     private func moveLabel(_ isUp: Bool) {
@@ -87,7 +127,7 @@ final class MBCTextField: UITextField {
                 guard let self = self else { return }
                 if isUp {
                     let offsetX = self.floatingLabel.frame.width * 0.1
-                    let translation = CGAffineTransform(translationX: -offsetX + 7, y: -self.frame.height/1.5)
+                    let translation = CGAffineTransform(translationX: -offsetX, y: -self.frame.height/1.5)
                     let scale = CGAffineTransform(scaleX: 0.8, y: 0.8)
                     self.floatingLabel.transform = translation.concatenating(scale)
                 } else {
@@ -98,32 +138,29 @@ final class MBCTextField: UITextField {
         )
     }
     
+    private func animateBorders(_ isUp: Bool) {
+        let animation = CABasicAnimation(keyPath: "path")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.toValue = isUp ? floatingPath.cgPath : defaultPath.cgPath
+        animation.duration = 0.0001
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.isRemovedOnCompletion = false
+        borderLayer.add(animation, forKey: "path")
+    }
+    
     @objc
-    private func start() {
+    private func startFloating() {
         if !isUp {
             isUp.toggle()
         }
     }
     
     @objc
-    private func end() {
+    private func endFloating() {
         if isUp && text.isEmptyOrNil {
             isUp.toggle()
         }
     }
-    
-    private func drawBezierPath() {
-        let path = UIBezierPath(roundedRect: bounds, cornerRadius: 4).cgPath
-        
-        let borderLayer = CAShapeLayer()
-        
-        borderLayer.path = path
-        borderLayer.lineWidth = 1
-        borderLayer.strokeColor = UIColor(named: C.Colors.brownishGreyTwo).unwrap.cgColor
-        
-        borderLayer.fillColor = UIColor.clear.cgColor
-        layer.addSublayer(borderLayer)
-    }
-
+     
 }
 
